6220d7947b7018ae60a0064950e8385dcf62fdc6
Gabriel Guedes <gabrielrguedess@gmail.com>
2021-06-08 02:48:11 -0300

    Ribbon Finished

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,45 @@
+import Button from 'components/Button';
+import Ribbon, { RibbonColor, RibbonSizes } from 'components/Ribbon';
+import * as S from './styles';
+
+export type BannerProps = {
+  img: string;
+  title: string;
+  subtitle: string;
+  buttonLabel: string;
+  buttonLink: string;
+
+  ribbon?: string;
+  ribbonSize?: RibbonSizes;
+  ribbonColor?: RibbonColor;
+};
+
+const Banner = ({
+  img,
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink,
+  ribbon,
+  ribbonColor = 'primary',
+  ribbonSize = 'normal',
+}: BannerProps) => (
+  <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon color={ribbonColor} size={ribbonSize}>
+        {ribbon}
+      </Ribbon>
+    )}
+    <S.Image src={img} role="img" aria-label={title} />
+
+    <S.Caption>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
+      <Button as="a" href={buttonLink} size="large">
+        {buttonLabel}
+      </Button>
+    </S.Caption>
+  </S.Wrapper>
+);
+
+export default Banner;
